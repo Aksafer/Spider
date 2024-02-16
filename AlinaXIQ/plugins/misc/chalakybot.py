2aa48d624d5587445e2160c9e39bc8d241c05943
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-02-16 14:47:04 +0300

    Update chalakybot.py

--- AlinaXIQ/plugins/misc/chalakybot.py
+++ AlinaXIQ/plugins/misc/chalakybot.py
@@ -13,7 +13,7 @@ userbot = Userbot()
 
 last_checked_time = None
 
-@app.on_message(command(["/botcheck","پشکنینی بوت","چالاکی بوت","پشکنینی بۆت","چالاکی بۆت"]))
+@app.on_message(command(["/botcheck","/botchk","پشکنینی بوت","چالاکی بوت","پشکنینی بۆت","چالاکی بۆت"]))
 async def check_bots_command(client, message):
     global last_checked_time
     try:
@@ -37,16 +37,16 @@ async def check_bots_command(client, message):
                 # Check if bot responded to /start message
                 async for bot_message in userbot.one.get_chat_history(bot_id, limit=1):
                     if bot_message.from_user.id == bot_id:
-                        response += f"**╭⎋ {bot.mention}\n l\n⇜ دۆخ: چالاك ✅**\n\n"
+                        response += f"**⇜ پشکنین بۆ {bot.mention} **\n l\n**⇜ بۆت: چالاکە ✅**\n\n"
                     else:
-                        response += f"**╭⎋ [{bot.mention}](tg://user?id={bot.id})\n l\n⇜ دۆخ: ناچالاك ❌**\n\n"
+                        response += f"**⇜ پشکنین بۆ [{bot.mention}](tg://user?id={bot.id})**\n l\n**⇜ بۆت: ناچالاکە ❌**\n\n"
             except Exception:
                 response += f"**╭⎋ {bot_username}\n l\n⇜ تۆ یوزەری بۆتی هەڵەت پێداوم تکایە دڵنیابەوە لەوەی کە یوزەرەکە ڕاستە**\n\n"
             # Update last checked time
             last_checked_time = start_time.strftime("%Y-%m-%d")
             await message.reply_text(f"**{response}⏲️ کۆتا پشکنین: {last_checked_time} **")
         else:
-            await message.reply_text("**⇜ فەرمانت هەڵە بەکارهێنا\n\n⇜ تکایە بەم شێوازە بیکە /botschk یوزەری بۆت**\n\n**⇜ نموونە :** `/botschk @IQM2BOT`")
+            await message.reply_text("**⇜ فەرمانت هەڵە بەکارهێنا\n\n⇜ تکایە بەم شێوازە بیکە /botcheck یوزەری بۆت**\n\n**⇜ نموونە :** `/botcheck @IQM2BOT`")
     except Exception as e:
         await message.reply_text(f"**⇜ هەڵەیەك ڕوویدا: {e}**")
         print(f"⇜ Error occurred during /botschk command: {e}**")
