e9f052fc1efb92968ae4026fe953045ddd8a66ea
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-02 23:05:47 +0300

    Update joingroup.py

--- AlinaXIQ/plugins/bot/joingroup.py
+++ AlinaXIQ/plugins/bot/joingroup.py
@@ -6,23 +6,28 @@ from pyrogram.errors import UserNotParticipant
 from AlinaXIQ import app
 
 channel = "EHS4SS"
+
+
 async def subscription(_, __: Client, message: Message):
-    try: await app.get_chat_member(channel, user_id)
-    except UserNotParticipant: return False
+    try:
+        await app.get_chat_member(channel, user_id)
+    except UserNotParticipant:
+        return False
     return True
-    
+
+
 subscribed = filters.create(subscription)
+
 
 @app.on_message(~subscribed)
 async def checker(_: Client, message: Message):
     if message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]: await message.delete()
-    user_id = message.from_user.id
+    await app.get_chat_member(message.chat.id, message.from_user.id)
     user = message.from_user.mentoin
     markup = Markup([
         [Button("𝗦𝗼𝘂𝗿𝗰𝗲 𝗔𝗹𝗶𝗻𝗮 🎻", url=f"https://t.me/{channel}")]
     ])
     await message.reply(
         f"**🧑🏻‍💻┋ ببورە ئەزیزم {user}؛\n🎻┋ پێویستە سەرەتا جۆینی کەناڵی بۆت بکەیت؛\n✅┋ کەناڵی بۆت : {channel}**",
-        reply_markup = markup
+        reply_markup=markup
     )
-    
