868bde97f1c4f4ddcf00922338719b68579312b1
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-02 23:12:55 +0300

    Update joingroup.py

--- AlinaXIQ/plugins/bot/joingroup.py
+++ AlinaXIQ/plugins/bot/joingroup.py
@@ -10,7 +10,7 @@ channel = "EHS4SS"
 
 async def subscription(_, __: Client, message: Message):
     try:
-        await app.get_chat_member(channel, user_id)
+        await app.get_chat_member(message.chat.id, message.from_user.id, channel)
     except UserNotParticipant:
         return False
     return True
@@ -22,7 +22,6 @@ subscribed = filters.create(subscription)
 @app.on_message(~subscribed)
 async def checker(_: Client, message: Message):
     if message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]: await message.delete()
-    await client.get_chat_member(message.chat.id, message.from_user.id)
     user = message.from_user.mentoin
     markup = Markup([
         [Button("𝗦𝗼𝘂𝗿𝗰𝗲 𝗔𝗹𝗶𝗻𝗮 🎻", url=f"https://t.me/{channel}")]
