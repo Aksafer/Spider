5e3d7b6114394bce7c8d68b27e1998afd6d5f02d
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-03-22 14:49:15 +0300

    Update group.py

--- AlinaXIQ/plugins/tools/group.py
+++ AlinaXIQ/plugins/tools/group.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from AlinaXIQ import app
+from AlinaXIQ.misc import SUDOERS
 from config import OWNER_ID
 from pyrogram.types import VideoChatEnded, Message
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
