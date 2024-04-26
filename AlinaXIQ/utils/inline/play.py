a402be606725eaa2fd7acd5891dacb1cac210583
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-04-26 21:55:50 +0300

    Update play.py

--- AlinaXIQ/utils/inline/play.py
+++ AlinaXIQ/utils/inline/play.py
@@ -446,34 +446,13 @@ def panel_markup_2(_, videoid, chat_id):
     ]
     return buttons
 
-def panel_markup_5(_, videoid, chat_id, played, dur):
-    played_sec = time_to_seconds(played)
-    duration_sec = time_to_seconds(dur)
-    percentage = (played_sec / duration_sec) * 100
-    umm = math.floor(percentage)
-    if 0 < umm <= 40:
-        bar = "◉——————————"
-    elif 10 < umm < 20:
-        bar = "—◉—————————"
-    elif 20 < umm < 30:
-        bar = "——◉————————"
-    elif 30 <= umm < 40:
-        bar = "———◉———————"
-    elif 40 <= umm < 50:
-        bar = "————◉——————"
-    elif 50 <= umm < 60:
-        bar = "——————◉————"
-    elif 50 <= umm < 70:
-        bar = "———————◉———"
-    else:
-        bar = "——————————◉"
-        
+def panel_markup_5(_, videoid, chat_id):
     buttons = [ 
 
          [
             InlineKeyboardButton(
-                text=f"{played} {bar} {dur}",
-                callback_data="GetTimer",
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
             ),
         ],
         [
