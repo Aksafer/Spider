de990391f34176276a395025c2d5768f83152159
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-04-26 14:58:28 +0300

    Update play.py

--- AlinaXIQ/plugins/play/play.py
+++ AlinaXIQ/plugins/play/play.py
@@ -744,7 +744,7 @@ from AlinaXIQ.utils.pastebin import AlinaBin
 from AlinaXIQ.utils.stream.queue import put_queue, put_queue_index
 from youtubesearchpython.__future__ import VideosSearch
 from AlinaXIQ.utils.thumbnails import get_thumb, get_qthumb
-from AlinaXIQ.utils.theme import check_theme
+
 
 
 async def stream(
@@ -830,8 +830,8 @@ async def stream(
                     "video" if video else "audio",
                     forceplay=forceplay,
                 )
-                theme = await check_theme(chat_id)
-                img = await get_thumb(vidid, user_id, theme)
+            
+                img = await get_thumb(vidid, user_id)
                 button = stream_markup(_, vidid, chat_id)
                 run = await app.send_photo(
                     original_chat_id,
@@ -889,9 +889,9 @@ async def stream(
                 user_id,
                 "video" if video else "audio",
             )
-            theme = await check_theme(chat_id)
+        
             position = len(db.get(chat_id)) - 1
-            qimg = await get_qthumb(vidid, user_id, theme)
+            qimg = await get_qthumb(vidid, user_id)
             button = aq_markup(_, chat_id)
             await app.send_photo(
                 chat_id=original_chat_id,
@@ -921,8 +921,8 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            theme = await check_theme(chat_id)
-            img = await get_thumb(vidid, user_id, theme)
+            
+            img = await get_thumb(vidid, user_id)
             button = stream_markup(_, vidid, chat_id)
             run = await app.send_photo(
                 original_chat_id,
@@ -1088,8 +1088,8 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            theme = await check_theme(chat_id)
-            img = await get_thumb(vidid, user_id, theme)
+            
+            img = await get_thumb(vidid, user_id)
             button = stream_markup2(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
