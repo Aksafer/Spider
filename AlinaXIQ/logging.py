f1a61f523c63b52c2510802193a3389305eebc00
iox7ssz <159122521+iox7ssz@users.noreply.github.com>
2024-05-01 13:13:38 +0300

    Update logging.py

--- AlinaXIQ/logging.py
+++ AlinaXIQ/logging.py
@@ -11,9 +11,10 @@ logging.basicConfig(
     ],
 )
 
-logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+logging.getLogger("ntgcalls").setLevel(logging.ERROR)
+logging.getLogger("httpx").setLevel(logging.ERROR)
 
 
 def LOGGER(name: str) -> logging.Logger:
