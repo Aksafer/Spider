9faf0324392e03b419fa4eb75e71617b30393080
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-05-04 22:22:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ SONG_DOWNLOAD_DURATION_LIMIT = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "99999
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from  on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 833360381))
+OWNER_ID = int(getenv("OWNER_ID", 6092147148))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -52,17 +52,17 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MGIMT")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/IQSUPP")
-OWNER_CHANNEL = getenv("OWNER_CHANNEL", "https://t.me/EHS4SS")
-GROUP_BOT = int(getenv("GROUP_BOT", "-1001962701094"))
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/YU_CQ")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/YR_HC")
+OWNER_CHANNEL = getenv("OWNER_CHANNEL", "https://t.me/YU_CQ")
+GROUP_BOT = int(getenv("GROUP_BOT", "-1002088468149"))
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
-SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "1c21247d714244ddbb09925dac565aed")
-SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "709e1a2969664491b58200860623ef19")
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "3497383a173bc579a19ae3ca2f8c6114")
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "3497383a173bc579a19ae3ca2f8c6114")
 
 
 # Maximum Limit Allowed for users to save playlists on bot's server
