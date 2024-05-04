e4b2378d94d3e91b69aebce730689896ad91ec7c
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-05-04 23:20:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/iox7ssz/d3keoeke",
+    "https://github.com/Aksafer/Spider",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
