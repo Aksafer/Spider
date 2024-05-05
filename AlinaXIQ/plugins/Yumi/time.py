0e559933ffcaf1d302a5fbafa72b39ef372c89f7
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-05-06 00:50:21 +0300

    Update time.py

--- AlinaXIQ/plugins/Yumi/time.py
+++ AlinaXIQ/plugins/Yumi/time.py
@@ -10,7 +10,7 @@ def get_current_time():
     current_time = datetime.now(tz)
     return current_time.strftime("%Y-%m-%d %H:%M:%S %Z%z")
 
-@app.on_message(command(["/Time","کات"]))
+@app.on_message(command(["/Time","التوقيت"]))
 def send_time(client, message):
     time = get_current_time()
-    client.send_message(message.chat.id, f"**کاتی کوردستان: {time}**")
+    client.send_message(message.chat.id, f"**توقيت القاهره: {time}**")
