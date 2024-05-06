0a0ea42dae27a4b65160a9e0e12291c3be2a0a11
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-05-06 03:36:16 +0300

    Update IQGO.py

--- AlinaXIQ/plugins/Yumi/IQGO.py
+++ AlinaXIQ/plugins/Yumi/IQGO.py
@@ -10,12 +10,12 @@ import random
 
 
 @app.on_message(
-    command(["گۆرانی"])
+    command(["اغنيه"])
 )
 async def music(client: Client, message: Message):
     rl = random.randint(1, 29)
-    url = f"https://t.me/ZWZZ7/{rl}"
-    await client.send_voice(message.chat.id, url, caption="**[⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝘼𝙇𝙄𝙉𝘼 - 🧑🏻‍💻🖤 گۆرانی](t.me/MGIMT)**\n\n**••┉┉┉┉┉••🝢••┉┉┉┉┉••**\n**¦  گۆرانییەکانم➧♥️**\n**@IQMUC - کەناڵی گۆرانی**",
+    url = f"https://t.me/YU_CQ/{rl}"
+    await client.send_voice(message.chat.id, url, caption="**[⧉• 𝙎𝙊𝙐𝙍𝘾𝙀 𝙀𝙍𝙊𝙍 - 🧑🏻‍💻🖤 اغنيه](t.me/YU_CQ)**\n\n**••┉┉┉┉┉••🝢••┉┉┉┉┉••**\n**¦  اغانيي➧♥**\n**@YU_CQ - قناة السورس**",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -26,27 +26,12 @@ async def music(client: Client, message: Message):
         )
     )
     
-@app.on_message(command(["وێنە","وێنەکان"]))
-async def ihd(client: Client, message: Message):
-    rs = random.randint(1,148)
-    url = f"https://t.me/GTTUTY/{rs}"
-    await client.send_photo(message.chat.id,url,caption="**[⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝘼𝙇𝙄𝙉𝘼 - 🧑🏻‍💻🖤 وێنەکان](t.me/MGIMT)**\n**••┉┉┉┉┉••🝢••┉┉┉┉┉••**\n\n**¦ وێنەکە دیاریکرا ♥•**",
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        )
-    )
 
-
-@app.on_message(command(["وێنەی کچان","کچان"]))
+@app.on_message(command(["صور بنات","صور"]))
 async def ihd(client: Client, message: Message):
     rl = random.randint(1,45)
-    url = f"https://t.me/ZSZZW/{rl}"
-    await client.send_photo(message.chat.id,url,caption="**[⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝘼𝙇𝙄𝙉𝘼 - 🧑🏻‍💻🖤 کچان](t.me/MGIMT)**\n**••┉┉┉┉┉••🝢••┉┉┉┉┉••**\n\n**¦ وێنەی کچان➧♥️\n@ZSZZW - کەناڵی وێنە**",
+    url = f"https://t.me/YU_CQ/{rl}"
+    await client.send_photo(message.chat.id,url,caption="**[⧉• 𝙎𝙊𝙐𝙍𝘾𝙀 𝙀𝙍𝙊𝙍 - 🧑🏻‍💻🖤 بنات](t.me/YU_CQ)**\n**••┉┉┉┉┉••🝢••┉┉┉┉┉••**\n\n**¦ صور بنات➧♥\n@YU_CQ - قناة السورس**",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -58,12 +43,12 @@ async def ihd(client: Client, message: Message):
     )
     
 @app.on_message(
-    command(["ق"])
+    command(["قراان"])
 )
 async def voice(client: Client, message: Message):
     rl = random.randint(1, 102)
-    url = f"https://t.me/IQQUR/{rl}"
-    await client.send_voice(message.chat.id, url, caption="**[⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝘼𝙇𝙄𝙉𝘼 - 🧑🏻‍💻🖤 قورئان](t.me/MGIMT)**\n**••┉┉┉┉┉••🝢••┉┉┉┉┉••**\n\n**¦ قورئانی پیرۆز➧♥️\n@IQQUR - کەناڵی قورئان**",
+    url = f"https://t.me/YU_CQ/{rl}"
+    await client.send_voice(message.chat.id, url, caption="**[⧉• 𝙎𝙊𝙐𝙍𝘾𝙀 𝙀𝙍𝙊𝙍 - 🧑🏻‍💻🖤 قران](t.me/YU_CQ)**\n**••┉┉┉┉┉••🝢••┉┉┉┉┉••**\n\n**¦ قران كريم➧♥️\n@YU_CQ - قناة السورس**",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -75,13 +60,13 @@ async def voice(client: Client, message: Message):
        )
   )
 
-@app.on_message(command([f"ڤیدیۆ", "v", "ڤ"])
+@app.on_message(command([f"فيديو", "v", "ڤ"])
 )
 async def video(client: Client, message: Message):
     rl = random.randint(5, 32)
-    u = await client.get_messages("IQVIDE",rl)
+    u = await client.get_messages("YU_CQ",rl)
     if u.video:
-     await client.send_video(message.chat.id, u.video.file_id, caption="**[⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝘼𝙇𝙄𝙉𝘼 - 🧑🏻‍💻🖤 ڤیدیۆ](t.me/MGIMT)**\n**••┉┉┉┉┉••🝢••┉┉┉┉┉••**\n\n**¦ @xv7amo - کەناڵی ڤیدیۆ♥️•**",
+     await client.send_video(message.chat.id, u.video.file_id, caption="**[⧉• 𝙎𝙊𝙐𝙍𝘾𝙀 𝙀𝙍𝙊𝙍 - 🧑🏻‍💻🖤 فيديوهات](t.me/YU_CQ)**\n**••┉┉┉┉┉••🝢••┉┉┉┉┉••**\n\n**¦ @YU_CQ - قناة السورس ♥•**",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -93,59 +78,22 @@ async def video(client: Client, message: Message):
        )
        )
 
-
-@app.on_message(
-    command(["ڕۆڵم"])
-    & filters.group
-)
-async def rotba(client, message):
-    dev = (OWNER_ID)
-    haya = (833360381,1818734394)
-    get = await client.get_chat_member(message.chat.id, message.from_user.id)
-    if message.from_user.id in haya:
-       rotba= "پڕۆگرامساز"
-    elif message.from_user.id in dev:
-        rotba = "خاوەنی بۆت"
-    elif get.status in [ChatMemberStatus.ADMINISTRATOR]:
-        rotba= "ئەدمین"
-    elif get.status in [ChatMemberStatus.OWNER]:
-        rotba= "سەرۆك"
-    else:
-         rotba = "ئەندام"
-    await message.reply_text(f"**ڕۆڵی تۆیە لەم گرووپە\n\nڕۆڵت ← « {rotba} »♥️**")
        
 
 bio = []
 
 @app.on_message(
-    command(["بایۆ"])
+    command(["بايو"])
     & filters.group
 )
 async def idjjdd(client, message:Message):
     if message.chat.id in bio:
       return
     usr = await client.get_chat(message.from_user.id)
-    await message.reply_text(f"**ئەوە بایۆیی تۆیە\n│ \n└ʙʏ: {usr.bio}**")
+    await message.reply_text(f"**هذه هي سيرتك الذاتية\n│ \n└ʙʏ: {usr.bio}**")
 
 
-@app.on_message(
-    command(["وێنەکەم"])
-    & filters.group
-)
-async def idjjdd(client, message):
-    if message.chat.id in iddof:
-      return
-    usr = await client.get_chat(message.from_user.id)
-    name = usr.first_name
-    i = ["0","10", "15","20", "25","30","35", "40","45", "50","55", "60"," 66", "70","77", "80","85", "90","99", "100","1000" ]
-    ik = random.choice(i)
-    photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"**ڕێژەی جوانیت \n│ \n└ʙʏ: {ik} %😂❤️**", 
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        name, url=f"https://t.me/{message.from_user.username}")
+
                 ],
             ]
         ),
