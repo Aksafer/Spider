aee060f3ebe18b03ec82a6f4cd3247362044f70c
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-05-07 01:04:44 +0300

    Update fonts.py

--- AlinaXIQ/plugins/Yumi/fonts.py
+++ AlinaXIQ/plugins/Yumi/fonts.py
@@ -43,7 +43,7 @@ async def style_buttons(c, m, cb=False):
             InlineKeyboardButton("H̆̈ă̈p̆̈p̆̈y̆̈", callback_data="style+happy"),
             InlineKeyboardButton("S̑̈ȃ̈d̑̈", callback_data="style+sad"),
         ],
-        [InlineKeyboardButton ("تحت",callback_data="close_reply"),InlineKeyboardButton ("لاحقاً ➻", callback_data="nxt")],
+        [InlineKeyboardButton ("اغلاق",callback_data="close_reply"),InlineKeyboardButton ("التالي ➻", callback_data="nxt")],
     ]
     if not cb:
         await m.reply_text(
@@ -96,7 +96,7 @@ async def nxt(c, m):
                 InlineKeyboardButton("S̶t̶r̶i̶k̶e̶", callback_data="style+strike"),
                 InlineKeyboardButton("F༙r༙o༙z༙e༙n༙", callback_data="style+frozen"),
             ],
-            [InlineKeyboardButton ("تحت",callback_data="close_reply"),InlineKeyboardButton ("خلف", callback_data="nxt+0")],
+            [InlineKeyboardButton ("اغلاق",callback_data="close_reply"),InlineKeyboardButton ("رجوع", callback_data="nxt+0")],
         ]
         await m.answer()
         await m.message.edit_reply_markup(InlineKeyboardMarkup(buttons))
