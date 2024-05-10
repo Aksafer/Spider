210f16499b3d688bc639f271680f5decca40fb24
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-05-10 09:57:40 +0300

    Update resume.py

--- AlinaXIQ/plugins/admins/resume.py
+++ AlinaXIQ/plugins/admins/resume.py
@@ -9,14 +9,14 @@ from AlinaXIQ.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(command(["resume", "cresume","/resume","/cresume","د","دەستپێکردنەوە"]) & ~filters.private & ~BANNED_USERS)
+@app.on_message(command(["كمل", "كملي"]) & ~BANNED_USERS)
 @AdminRightsCheck
 async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    user_mention = message.from_user.mention if message.from_user else "𝖠𝖽𝗆𝗂𝗇"
-    await Alina.resume_stream(chat_id)
+    user_mention = message.from_user.mention if message.from_user else "المشـرف"
+    await Zelzaly.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"].format(user_mention), reply_markup=close_markup(_)
     )
