a77449ca0d574b6f4e8d25e7d69e33365428975a
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-05-11 04:54:02 +0300

    Update download.py

--- AlinaXIQ/plugins/play/download.py
+++ AlinaXIQ/plugins/play/download.py
@@ -12,7 +12,7 @@ from pyrogram.types import Message, InputTextMessageContent
 from youtube_search import YoutubeSearch
 
 from AlinaXIQ import app
-from AlinaXIQ.plugins.play import command
+from AlinaXIQ import command
 
 def remove_if_exists(path):
     if os.path.exists(path):
