14f4be27662f8948d1999ea4d039e192765da2fa
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-05-11 06:25:50 +0300

    Update ownerr.py

--- AlinaXIQ/plugins/play/ownerr.py
+++ AlinaXIQ/plugins/play/ownerr.py
@@ -138,33 +138,3 @@ async def khfzss(client: Client, message: Message):
 
 
 
-
-
-@app.on_message(filters.command(["تحويل لصوره"], ""))
-
-async def elkatifh(client: Client, message: Message):
-
-    reply = message.reply_to_message
-
-    if not reply:
-
-        return await message.reply("الرد على ملصق.")
-
-    if not reply.sticker:
-
-        return await message.reply("الرد على ملصق.")
-
-    m = await message.reply("يتم المعالجه..")
-
-    f = await reply.download(f"{reply.sticker.file_unique_id}.png")
-
-    await gather(*[message.reply_photo(f),message.reply_document(f)])
-
-    await m.delete()
-
-    os.remove(f)
-
-
-
-
-
